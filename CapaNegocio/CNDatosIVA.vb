9d850a9fb906befd67d7c350a1a1e5d70bedf8e0
Ainara <ikawv@plaiaundi.net>
2020-04-30 17:36:49 +0200

    Capa Negocios: parte iva

---
+++ CapaNegocio/CNDatosIVA.vb
@@ -0,0 +1,12 @@
+﻿Imports CapaDatos
+
+Public Class CNDatosIVA
+    ' CREAMOS EL OBJETO DE CAPA DATOS
+    Dim objCapaDatos As New CDDatosIVA
+
+    ' IMPLEMENTAMOS LA FUNCIÓN QUE DEVUELVE LA LISTA DE IVA
+    Public Function ObtenerIVA() As DataTable
+        Return objCapaDatos.QrylistarIVA
+    End Function
+
+End Class
