f05751b5c36d63342d88d340c18fae97ab35223c
Ainara <ikawv@plaiaundi.net>
2020-04-30 17:32:42 +0200

    Capa datos: parte del iva

---
+++ CapaDatos/CDConexion.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class CDConexion
+
+    Public estado As String
+    Public Function getConnection() As OleDbConnection
+        Dim connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\..\..\DatabaseHeladeria.accdb;Persist Security Info=False"
+        getConnection = New OleDbConnection(connString)
+    End Function
+
+
+End Class
