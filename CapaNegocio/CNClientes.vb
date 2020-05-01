ae29dd84902fdca2595c6b65369a3abe27b303c1
Ainara Vesga <ikawv@plaiaundi.net>
2020-05-01 17:31:34 +0200

    CN clientes creado

---
+++ CapaNegocio/CNClientes.vb
@@ -0,0 +1,12 @@
+﻿Imports CapaDatos
+
+Public Class CNClientes
+    ' Creamos el objeto de la capa datos
+    Dim objCapaDatos As New CDClientes
+
+    ' Funcion que devuelve la lista de clientes
+    Public Function ObtenerClientes() As DataTable
+        Return objCapaDatos.QryListarClientes
+    End Function
+
+End Class
