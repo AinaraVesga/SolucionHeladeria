d5bc63f0a18a60fb4549c2540461321f3bfbb2e7
Ainara Vesga <ikawv@plaiaundi.net>
2020-05-02 11:39:39 +0200

    CapaEntidad "Facturacion" creada

--- CapaEntidad/CEFacturacion.vb
+++ CapaEntidad/CEFacturacion.vb
@@ -1,3 +1,21 @@
 ﻿Public Class CEFacturacion
+    ' Atributos
+    Private _idfacturacion As String
+    Private _frecuencia As String
+
+    ' Constructora
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(idfacturacion As String, frecuencia As String)
+        _idfacturacion = idfacturacion
+        _frecuencia = frecuencia
+    End Sub
+
+    Public Sub New(row As DataRow)
+        _idfacturacion = row("IDFACTURACION")
+        _frecuencia = row("FRECUENCIA")
+    End Sub
 
 End Class
